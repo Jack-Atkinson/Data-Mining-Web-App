75861baa20ee27c339a64b5b179b56f860b57588
Jack Atkinson <jack.atkinson@outlook.com>
2015-11-30 15:27:11 -0600

    added some notes

---
+++ Reinders Data Mining Web App/Models/WebFilterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reinders_Data_Mining_Web_App.Models
+{
+    public class WebFilterModel
+    {
+        public string[] elementTree { get; set; }
+        public string Signature { get; set; }
+        public string EndMarker { get; set; }
+        //http://stackoverflow.com/questions/21288462/c-sharp-mvc-4-passing-javascript-array-in-view-to-controller
+    }
+}
